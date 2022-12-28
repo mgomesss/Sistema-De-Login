c534e10f075c8e266b805790dce1b5112fd78656
Matheus Gomes <matheusgomes710@gmail.com>
2022-12-28 00:10:47 -0300

    Exibindo informações de usuário logado
    Mostrando informações do usuario logado na pagina hoje, passando atravez de viewbags, e puxando os dados de cookies criados no momento do login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,15 @@ namespace WebApplication1.Controllers
         {
             if (Session["Logado"] != null && Session["Logado"].ToString() == "OK")
             {
+                ViewBag.Id_Usuario = Request.Cookies["inf_user"]["Id_Usuario"];
+                ViewBag.NomeUsuario = Request.Cookies["inf_user"]["NomeUsuario"];
+                ViewBag.Email = Request.Cookies["inf_user"]["Email"];
+                ViewBag.Bloqueio = Request.Cookies["inf_user"]["Bloqueio"];
+                ViewBag.FK_Grupo = Request.Cookies["inf_user"]["FK_Grupo"];
+                ViewBag.Dt_Criacao = Request.Cookies["inf_user"]["Dt_Criacao"];
+                ViewBag.Dt_AlteracaoSenha = Request.Cookies["inf_user"]["Dt_AlteracaoSenha"];
+                ViewBag.Dt_Bloqueio = Request.Cookies["inf_user"]["Dt_Bloqueio"];
+                
                 return View();
             }
             
